f06cbb6280fcc1c45e026f276b45562214e808a4
d0ntay <165851090+d0ntay@users.noreply.github.com>
2024-06-15 16:31:31 -0400

    Update active_hosts.py
    i think this should work

--- network_tools/active_hosts.py
+++ network_tools/active_hosts.py
@@ -1,2 +1,22 @@
+from scapy import *
+
 def active_hosts():
-    print('ello there')
+    hosts = []
+    print('Please enter network bits in decimal format. Ex. 192.168.1 ')
+    network = input('>')
+    ips = [network + '.'+ i for i in range(1,256)]
+    hosts = scan(ips)
+    for host in hosts:
+        print('[*]' + host)
+
+def scan(ips):
+    active_hosts = []
+    while True:
+        print("[*] Scanning...")
+    for ip in ips:
+        icmp = IP(dst=ip)/ICMP()
+        reply = sr1(icmp, timeout=2, verbose=False)
+        if reply is not None:
+            active_hosts.append(ip)
+
+return active_hosts
